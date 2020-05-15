2dafb1e21250ee3a5d5b83d187865e248a7a3242
Chigusa Izumi <chigusa.f124@gmail.com>
2020-05-15 18:44:44 +0900

    Initial Commit

---
+++ techmon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  techmon
+//
+//  Created by 泉 千種 on 2020/05/15.
+//  Copyright © 2020 Izumi zu-mi-. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
